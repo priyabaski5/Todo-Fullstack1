058b59534e22a0181389649835878670d042a274
Gnanapriya <gnanapriyabaskar@gmail.com>
2025-06-04 19:34:27 +0530

    Added frontend folder as normal part of the project

---
+++ backend/Server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const routes = require('./routes/TodoRouter');
+
+require('dotenv').config();
+const app = express();
+const PORT = process.env.PORT || 8000;
+
+app.use(express.json());
+const allowedOrigins = [
+    'https://ornate-monstera-538874.netlify.app',
+    'http://localhost:3000' ];
+app.use(cors({
+    origin: allowedOrigins
+}));
+
+mongoose
+    .connect(process.env.Mongodb_URL)
+    .then(()=>console.log(`Connected to mongodb...`))
+    .catch((err)=>console.log(err));
+
+app.use(routes);
+app.listen(PORT,()=> console.log(`Listening to PORT: ${PORT}`));
---
+++ frontend/src/utils/HandleApi.js
@@ -0,0 +1,48 @@
+//To get acccess from the backend and retrieve data we use  axios
+
+import axios from 'axios'
+
+const baseURL = "http://localhost:8000"
+
+const getAllTodo = (setTodo) =>
+{
+    axios
+    .get(`${baseURL}`)
+    .then(({data}) => {
+        console.log('data => ',data);
+        setTodo(data)
+    })
+    .catch((err) => console.log(err))
+}
+
+const addTodo = (text, setText, setTodo) => {
+    axios
+    .post(`${baseURL}/save`, {text})
+    .then((data)=>{
+        console.log(data)
+        setText("")
+        getAllTodo(setTodo)
+    })
+    .catch((err) => console.log(err))
+}
+
+const updateTodo = (todoId, text, setText, setTodo, setUpdate) => {
+    axios
+    .post(`${baseURL}/update`, { _id : todoId,text})
+    .then((data)=>{
+        setText("")
+        setUpdate(false)
+        getAllTodo(setTodo)
+    })
+    .catch((err) => console.log(err))
+}
+
+const deleteTodo = (_id, setTodo) => {
+    axios.post(`${baseURL}/delete`, {_id})
+    .then((data)=> {
+        getAllTodo(setTodo)
+    })
+    .catch((err) => console.log(err))
+}
+
+export {getAllTodo, addTodo, updateTodo, deleteTodo}
